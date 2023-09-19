d9f5bd50e8e5a8cfd3e16777d0f2dcac5bf9355c
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-09-19 09:29:28 +0300

    Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(name){
+    const catsCopy = [...cats, name];
+    return catsCopy
+}
+
+function prependCat(name){
+    const catsCopy = [name, ...cats];
+    return catsCopy
+}
+
+function removeLastCat(name){
+    const catsCopy = cats.slice(0, cats.length -1)
+    return catsCopy
+}
+
+function removeFirstCat(name){
+    const catsCopy =  cats.slice(1)
+    return catsCopy
+}
